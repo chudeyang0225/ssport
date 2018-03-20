8be65d3bb534d4b23611c7cf5cc3095cec11eb8f
chudeyang0225 <chudeyang0225@gwu.edu>
2018-03-20 10:32:08 -0400

    optimization

--- update-git.sh
+++ update-git.sh
@@ -8,5 +8,6 @@ cd git-2.7.2
 make prefix=/usr/local/git all
 make prefix=/usr/local/git install
 echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/profile
-source /etc/profile
 git config --global http.sslversion tlsv1
+cd .. && rm -rf git-2.7*
+source /etc/profile
